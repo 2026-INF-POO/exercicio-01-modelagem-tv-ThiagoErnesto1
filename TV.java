11894649c74cc4f603d607a8e95688795c106c94
ThiagoErnesto1 <20251ti0086@ifba.edu.br>
2026-03-03 09:09:50 -0300

    Update TV.java

--- TV.java
+++ TV.java
@@ -1,1 +1,37 @@
-
+class TV{
+  int polegadas;
+  int volume=5;
+  String marca;
+  int voltagem;
+  int canal;
+  int ligar() {
+    if (
+  }
+  void desligar() {
+    // Lógica para desligar tv
+  }
+  int aumentarvol() {
+    if (volume<10){
+      volume++;
+    }
+    return volume;
+  }
+  int diminuirvol(){
+    if (volume>1){
+      volume--;
+    }
+    return volume;
+  }  
+  void canalmais(){
+    if (canal<10){
+      canal++;
+    }
+    return canal;
+  }
+  void canalmenos(){
+    if (canal>1){
+      canal--;
+    }  
+    return canal;
+  } 
+}
